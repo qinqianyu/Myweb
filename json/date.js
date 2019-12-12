fa2020e1eed48c55b0efff445219f382d376778b
jiaxiangkai <2410992642@qq.com>
2019-12-12 16:20:55 +0800

    12.12.01

---
+++ json/date.js
@@ -0,0 +1,96 @@
+val jasondata={
+  "nodes": [
+    {
+      "name": "小米",
+      "lable": "Company",
+      "id": "123457"
+    },
+    {
+      "name": "美团",
+      "lable": "Company",
+      "id": "123460"
+    },
+    {
+      "name": "雷军",
+      "lable": "Person",
+      "id": "00001"
+    },
+    {
+      "name": null,
+      "lable": "Company",
+      "id": "123458"
+    },
+    {
+      "name": "马化腾",
+      "lable": "Person",
+      "id": "00003"
+    },
+    {
+      "name": null,
+      "lable": "Company",
+      "id": "123459"
+    },
+    {
+      "name": "马云",
+      "lable": "Person",
+      "id": "00004"
+    },
+    {
+      "name": "李斌",
+      "lable": "Person",
+      "id": "00002"
+    }
+  ],
+  "links": [
+    {
+      "type": "投资",
+      "source": "123457",
+      "target": "123460"
+    },
+    {
+      "type": "总裁",
+      "source": "123457",
+      "target": "00001"
+    },
+    {
+      "type": "总经理",
+      "source": "123458",
+      "target": "00001"
+    },
+    {
+      "type": "CEO",
+      "source": "123458",
+      "target": "00003"
+    },
+    {
+      "type": "投资",
+      "source": "00003",
+      "target": "123458"
+    },
+    {
+      "type": "投资",
+      "source": "00001",
+      "target": "123458"
+    },
+    {
+      "type": "CEO",
+      "source": "123459",
+      "target": "00001"
+    },
+    {
+      "type": "投资",
+      "source": "00001",
+      "target": "123457"
+    },
+    {
+      "type": "投资",
+      "source": "00004",
+      "target": "123458"
+    },
+    {
+      "type": "CEO",
+      "source": "123457",
+      "target": "00002"
+    }
+  ]
+}
